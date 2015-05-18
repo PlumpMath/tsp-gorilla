0d41255a60091ed7ffa108301f73d134c40ea64d
sooheon <tngjs0@gmail.com>
2015-05-18 20:01:48 +0900

    tour-length done, TODO Representing Cities section

--- src/tsp/gorilla_notebook.clj
+++ src/tsp/gorilla_notebook.clj
@@ -28,22 +28,12 @@
 ;;; - **What is ...**: We can define a function to answer the question *what is the shortest possible tour?*  The function takes a set of cities as input and returns a tour as output. I will use the convention that any such function will have a name ending in the letters "`tsp`", the traditional abbreviation for Traveling Salesperson Problem.
 ;;;
 ;;; At this stage I have a rough sketch of how to attack the problem.  I don't have all the answers, and I haven't committed to specific representations for all the concepts, but I know what all the pieces are, and I don't see anything that stops me from proceeding."
-
+;;;
 ;; **
 
 ;; **
 ;;; Let's begin by populating the namespace:
 ;; **
-
-;; @@
-(ns tsp.core
-  (:require [clojure.math.combinatorics :as combo]
-            [clojure.math.numeric-tower :as math]
-            [gorilla-plot.core :as gp]))
-;; @@
-;; =>
-;;; {"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}
-;; <=
 
 ;; **
 ;;; ### All Tours Algorithm `alltours-tsp`
@@ -52,13 +42,18 @@
 ;;;
 ;;; In general our design philosophy is to first write an English description of the algorithm, then write Clojure code that closely mirrors the English description. This will probably require some auxilliary functions and data structures; just assume they exist; devlare them in a forward declaration, and eventually define them with the same design philosophy.
 ;;;
-;;; Here is the start of the implementation. We declare the functions we can see we will need in the future, and we can refer to them in higher level functions without defining them ahead of time.
+;;; Here is the start of the implementation. We declare the functions we will need in the future, and we can refer to them in higher level functions without defining them ahead of time. This serves as a kind of TODO list for implementing later.
 ;; **
 
 ;; @@
-(declare alltours-tsp
-         alltours      ; Declared because alltours-tsp refers to it
-         shortest-tour
+(ns tsp.core
+  (:require [clojure.math.combinatorics :as combo]
+            [clojure.math.numeric-tower :as math]
+            [gorilla-plot.core :as gp]))
+;; @@
+
+;; @@
+(declare alltours      ; Declared because alltours-tsp refers to it
          tour-length)  ; Same as above--we will implement them later
 
 (defn alltours-tsp
@@ -78,6 +73,7 @@
 ;;;
 ;;; **Note**: Peter explains that he considered using [1 2 3 1] as the representation of this tour, as well as an ordered list of edges between cities: [[1 2] [2 3] [3 1]], but decided that [1 2 3] was simplest.
 ;; **
+
 ;; @@
 (defn alltours
   "Generate all possible tours."
@@ -99,16 +95,24 @@
 ;; **
 
 ;; @@
-(declare tour-length
-         distance)
+(declare distance) ; Because it is referenced but not yet defined
 
 (defn tour-length
   "Sum of distances between each pair of consecutive cities in the tour."
   [tour]
-  (let [dests (concat (drop 1 tour) [(first tour)])
-        city-pairs (map vector tour dests)]
+  (let [destinations (lazy-cat (drop 1 tour) (take 1 tour))
+        city-pairs (map vector tour destinations)]
     (apply + (map distance city-pairs))))
 ;; @@
+
+;; **
+;;; This function binds `destinations` as a list of the tour cities rotated by 1. Then `city-pairs` is created by mapping through the `tour` and `destinations` lists and building a list of vectors. Given [1 2 3], `city-pairs` looks like [[1 2] [2 3] [3 1]].
+;; **
+
+;; **
+;;; ### Representing Cities
+;;; 
+;; **
 
 ;; @@
 (defn distance
@@ -119,9 +123,6 @@
     (math/sqrt (+ (math/expt x-dist 2)
                   (math/expt y-dist 2)))))
 ;; @@
-;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;tsp.core/alltours-tsp</span>","value":"#'tsp.core/alltours-tsp"}
-;; <=
 
 ;; @@
 (defrecord City [x y])
@@ -130,9 +131,6 @@
   (for [i (range n)]
     (City. (rand-int w) (rand-int h))))
 ;; @@
-;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;tsp.core/gen-cities</span>","value":"#'tsp.core/gen-cities"}
-;; <=
 
 ;; @@
 (alltours [1 2 3])
@@ -141,16 +139,10 @@
 ;; @@
 (tour-length (alltours-tsp (gen-cities 3)))
 ;; @@
-;; =>
-;;; {"type":"html","content":"<span class='clj-double'>634.6747913115241</span>","value":"634.6747913115241"}
-;; <=
 
 ;; @@
 (alltours-tsp (gen-cities 3))
 ;; @@
-;; =>
-;;; {"type":"list-like","open":"<span class='clj-lazy-seq'>(</span>","close":"<span class='clj-lazy-seq'>)</span>","separator":" ","items":[{"type":"list-like","open":"<span class='clj-record'>#tsp.core.City{</span>","close":"<span class='clj-record'>}</span>","separator":" ","items":[{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:x</span>","value":":x"},{"type":"html","content":"<span class='clj-unkown'>178</span>","value":"178"}],"value":"[:x 178]"},{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:y</span>","value":":y"},{"type":"html","content":"<span class='clj-unkown'>145</span>","value":"145"}],"value":"[:y 145]"}],"value":"#tsp.core.City{:x 178, :y 145}"},{"type":"list-like","open":"<span class='clj-record'>#tsp.core.City{</span>","close":"<span class='clj-record'>}</span>","separator":" ","items":[{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:x</span>","value":":x"},{"type":"html","content":"<span class='clj-unkown'>693</span>","value":"693"}],"value":"[:x 693]"},{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:y</span>","value":":y"},{"type":"html","content":"<span class='clj-unkown'>260</span>","value":"260"}],"value":"[:y 260]"}],"value":"#tsp.core.City{:x 693, :y 260}"},{"type":"list-like","open":"<span class='clj-record'>#tsp.core.City{</span>","close":"<span class='clj-record'>}</span>","separator":" ","items":[{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:x</span>","value":":x"},{"type":"html","content":"<span class='clj-unkown'>91</span>","value":"91"}],"value":"[:x 91]"},{"type":"list-like","open":"","close":"","separator":" ","items":[{"type":"html","content":"<span class='clj-keyword'>:y</span>","value":":y"},{"type":"html","content":"<span class='clj-unkown'>32</span>","value":"32"}],"value":"[:y 32]"}],"value":"#tsp.core.City{:x 91, :y 32}"}],"value":"(#tsp.core.City{:x 178, :y 145} #tsp.core.City{:x 693, :y 260} #tsp.core.City{:x 91, :y 32})"}
-;; <=
 
 ;; @@
 (defn city->vec [city]
@@ -163,16 +155,10 @@
     (gp/compose (gp/list-plot tourv :joined true)
                 (gp/list-plot tourv :symbol-size 70))))
 ;; @@
-;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;tsp.core/plot-tour</span>","value":"#'tsp.core/plot-tour"}
-;; <=
 
 ;; @@
 (plot-tour (alltours-tsp (gen-cities 6)))
 ;; @@
-;; =>
-;;; {"type":"vega","content":{"width":400,"height":247.2187957763672,"padding":{"bottom":20,"top":10,"right":10,"left":50},"scales":[{"name":"x","type":"linear","range":"width","zero":false,"domain":{"data":"e047d70f-2572-4e5e-9678-396d20ca8bf4","field":"data.x"}},{"name":"y","type":"linear","range":"height","nice":true,"zero":false,"domain":{"data":"e047d70f-2572-4e5e-9678-396d20ca8bf4","field":"data.y"}}],"axes":[{"scale":"x","type":"x"},{"scale":"y","type":"y"}],"data":[{"name":"e047d70f-2572-4e5e-9678-396d20ca8bf4","values":[{"x":448,"y":471},{"x":149,"y":451},{"x":294,"y":138},{"x":817,"y":231},{"x":766,"y":271},{"x":674,"y":407},{"x":448,"y":471}]},{"name":"ba0adcb1-da81-4633-8c44-b221694528bd","values":[{"x":448,"y":471},{"x":149,"y":451},{"x":294,"y":138},{"x":817,"y":231},{"x":766,"y":271},{"x":674,"y":407},{"x":448,"y":471}]}],"marks":[{"type":"line","from":{"data":"e047d70f-2572-4e5e-9678-396d20ca8bf4"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"y":{"scale":"y","field":"data.y"},"stroke":{"value":"#FF29D2"},"strokeWidth":{"value":2},"strokeOpacity":{"value":1}}}},{"type":"symbol","from":{"data":"ba0adcb1-da81-4633-8c44-b221694528bd"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"y":{"scale":"y","field":"data.y"},"fill":{"value":"steelblue"},"fillOpacity":{"value":1}},"update":{"shape":"circle","size":{"value":70},"stroke":{"value":"transparent"}},"hover":{"size":{"value":210},"stroke":{"value":"white"}}}}]},"value":"#gorilla_repl.vega.VegaView{:content {:width 400, :height 247.2188, :padding {:bottom 20, :top 10, :right 10, :left 50}, :scales [{:name \"x\", :type \"linear\", :range \"width\", :zero false, :domain {:data \"e047d70f-2572-4e5e-9678-396d20ca8bf4\", :field \"data.x\"}} {:name \"y\", :type \"linear\", :range \"height\", :nice true, :zero false, :domain {:data \"e047d70f-2572-4e5e-9678-396d20ca8bf4\", :field \"data.y\"}}], :axes [{:scale \"x\", :type \"x\"} {:scale \"y\", :type \"y\"}], :data ({:name \"e047d70f-2572-4e5e-9678-396d20ca8bf4\", :values ({:x 448, :y 471} {:x 149, :y 451} {:x 294, :y 138} {:x 817, :y 231} {:x 766, :y 271} {:x 674, :y 407} {:x 448, :y 471})} {:name \"ba0adcb1-da81-4633-8c44-b221694528bd\", :values ({:x 448, :y 471} {:x 149, :y 451} {:x 294, :y 138} {:x 817, :y 231} {:x 766, :y 271} {:x 674, :y 407} {:x 448, :y 471})}), :marks ({:type \"line\", :from {:data \"e047d70f-2572-4e5e-9678-396d20ca8bf4\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :stroke {:value \"#FF29D2\"}, :strokeWidth {:value 2}, :strokeOpacity {:value 1}}}} {:type \"symbol\", :from {:data \"ba0adcb1-da81-4633-8c44-b221694528bd\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :fill {:value \"steelblue\"}, :fillOpacity {:value 1}}, :update {:shape \"circle\", :size {:value 70}, :stroke {:value \"transparent\"}}, :hover {:size {:value 210}, :stroke {:value \"white\"}}}})}}"}
-;; <=
 
 ;; @@
 (defn valid-tour
@@ -189,21 +175,10 @@
      (println (count tour) "city tour with length"
               (tour-length tour) "for" (str algorithm))]))
 ;; @@
-;; =>
-;;; {"type":"html","content":"<span class='clj-var'>#&#x27;tsp.core/plot-tsp</span>","value":"#'tsp.core/plot-tsp"}
-;; <=
 
 ;; @@
 (plot-tsp alltours-tsp (gen-cities 6))
 ;; @@
-;; ->
-;;; &quot;Elapsed time: 113.42818 msecs&quot;
-;;; 6 city tour with length 1599.608460755042 for tsp.core$all_tours_tsp@55ec4168
-;;;
-;; <-
-;; =>
-;;; {"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"vega","content":{"width":400,"height":247.2187957763672,"padding":{"bottom":20,"top":10,"right":10,"left":50},"scales":[{"name":"x","type":"linear","range":"width","zero":false,"domain":{"data":"c95a83bb-c179-4bef-8c9c-df0972944017","field":"data.x"}},{"name":"y","type":"linear","range":"height","nice":true,"zero":false,"domain":{"data":"c95a83bb-c179-4bef-8c9c-df0972944017","field":"data.y"}}],"axes":[{"scale":"x","type":"x"},{"scale":"y","type":"y"}],"data":[{"name":"c95a83bb-c179-4bef-8c9c-df0972944017","values":[{"x":253,"y":538},{"x":415,"y":353},{"x":614,"y":144},{"x":467,"y":14},{"x":452,"y":20},{"x":61,"y":137},{"x":253,"y":538}]},{"name":"70b05175-3194-4778-9842-e8a6bec7d869","values":[{"x":253,"y":538},{"x":415,"y":353},{"x":614,"y":144},{"x":467,"y":14},{"x":452,"y":20},{"x":61,"y":137},{"x":253,"y":538}]}],"marks":[{"type":"line","from":{"data":"c95a83bb-c179-4bef-8c9c-df0972944017"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"y":{"scale":"y","field":"data.y"},"stroke":{"value":"#FF29D2"},"strokeWidth":{"value":2},"strokeOpacity":{"value":1}}}},{"type":"symbol","from":{"data":"70b05175-3194-4778-9842-e8a6bec7d869"},"properties":{"enter":{"x":{"scale":"x","field":"data.x"},"y":{"scale":"y","field":"data.y"},"fill":{"value":"steelblue"},"fillOpacity":{"value":1}},"update":{"shape":"circle","size":{"value":70},"stroke":{"value":"transparent"}},"hover":{"size":{"value":210},"stroke":{"value":"white"}}}}]},"value":"#gorilla_repl.vega.VegaView{:content {:width 400, :height 247.2188, :padding {:bottom 20, :top 10, :right 10, :left 50}, :scales [{:name \"x\", :type \"linear\", :range \"width\", :zero false, :domain {:data \"c95a83bb-c179-4bef-8c9c-df0972944017\", :field \"data.x\"}} {:name \"y\", :type \"linear\", :range \"height\", :nice true, :zero false, :domain {:data \"c95a83bb-c179-4bef-8c9c-df0972944017\", :field \"data.y\"}}], :axes [{:scale \"x\", :type \"x\"} {:scale \"y\", :type \"y\"}], :data ({:name \"c95a83bb-c179-4bef-8c9c-df0972944017\", :values ({:x 253, :y 538} {:x 415, :y 353} {:x 614, :y 144} {:x 467, :y 14} {:x 452, :y 20} {:x 61, :y 137} {:x 253, :y 538})} {:name \"70b05175-3194-4778-9842-e8a6bec7d869\", :values ({:x 253, :y 538} {:x 415, :y 353} {:x 614, :y 144} {:x 467, :y 14} {:x 452, :y 20} {:x 61, :y 137} {:x 253, :y 538})}), :marks ({:type \"line\", :from {:data \"c95a83bb-c179-4bef-8c9c-df0972944017\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :stroke {:value \"#FF29D2\"}, :strokeWidth {:value 2}, :strokeOpacity {:value 1}}}} {:type \"symbol\", :from {:data \"70b05175-3194-4778-9842-e8a6bec7d869\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :fill {:value \"steelblue\"}, :fillOpacity {:value 1}}, :update {:shape \"circle\", :size {:value 70}, :stroke {:value \"transparent\"}}, :hover {:size {:value 210}, :stroke {:value \"white\"}}}})}}"},{"type":"html","content":"<span class='clj-nil'>nil</span>","value":"nil"}],"value":"[#gorilla_repl.vega.VegaView{:content {:width 400, :height 247.2188, :padding {:bottom 20, :top 10, :right 10, :left 50}, :scales [{:name \"x\", :type \"linear\", :range \"width\", :zero false, :domain {:data \"c95a83bb-c179-4bef-8c9c-df0972944017\", :field \"data.x\"}} {:name \"y\", :type \"linear\", :range \"height\", :nice true, :zero false, :domain {:data \"c95a83bb-c179-4bef-8c9c-df0972944017\", :field \"data.y\"}}], :axes [{:scale \"x\", :type \"x\"} {:scale \"y\", :type \"y\"}], :data ({:name \"c95a83bb-c179-4bef-8c9c-df0972944017\", :values ({:x 253, :y 538} {:x 415, :y 353} {:x 614, :y 144} {:x 467, :y 14} {:x 452, :y 20} {:x 61, :y 137} {:x 253, :y 538})} {:name \"70b05175-3194-4778-9842-e8a6bec7d869\", :values ({:x 253, :y 538} {:x 415, :y 353} {:x 614, :y 144} {:x 467, :y 14} {:x 452, :y 20} {:x 61, :y 137} {:x 253, :y 538})}), :marks ({:type \"line\", :from {:data \"c95a83bb-c179-4bef-8c9c-df0972944017\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :stroke {:value \"#FF29D2\"}, :strokeWidth {:value 2}, :strokeOpacity {:value 1}}}} {:type \"symbol\", :from {:data \"70b05175-3194-4778-9842-e8a6bec7d869\"}, :properties {:enter {:x {:scale \"x\", :field \"data.x\"}, :y {:scale \"y\", :field \"data.y\"}, :fill {:value \"steelblue\"}, :fillOpacity {:value 1}}, :update {:shape \"circle\", :size {:value 70}, :stroke {:value \"transparent\"}}, :hover {:size {:value 210}, :stroke {:value \"white\"}}}})}} nil]"}
-;; <=
 
 ;; **
 ;;; ### All Non-Redundant Tours Algorithm (improved `alltours-tsp`)
@@ -213,9 +188,6 @@
 ;; @@
 (alltours [1 2 3])
 ;; @@
-;; =>
-;;; {"type":"list-like","open":"<span class='clj-lazy-seq'>(</span>","close":"<span class='clj-lazy-seq'>)</span>","separator":" ","items":[{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-long'>1</span>","value":"1"},{"type":"html","content":"<span class='clj-long'>2</span>","value":"2"},{"type":"html","content":"<span class='clj-long'>3</span>","value":"3"}],"value":"[1 2 3]"},{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-long'>1</span>","value":"1"},{"type":"html","content":"<span class='clj-long'>3</span>","value":"3"},{"type":"html","content":"<span class='clj-long'>2</span>","value":"2"}],"value":"[1 3 2]"},{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-long'>2</span>","value":"2"},{"type":"html","content":"<span class='clj-long'>1</span>","value":"1"},{"type":"html","content":"<span class='clj-long'>3</span>","value":"3"}],"value":"[2 1 3]"},{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-long'>2</span>","value":"2"},{"type":"html","content":"<span class='clj-long'>3</span>","value":"3"},{"type":"html","content":"<span class='clj-long'>1</span>","value":"1"}],"value":"[2 3 1]"},{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-long'>3</span>","value":"3"},{"type":"html","content":"<span class='clj-long'>1</span>","value":"1"},{"type":"html","content":"<span class='clj-long'>2</span>","value":"2"}],"value":"[3 1 2]"},{"type":"list-like","open":"<span class='clj-vector'>[</span>","close":"<span class='clj-vector'>]</span>","separator":" ","items":[{"type":"html","content":"<span class='clj-long'>3</span>","value":"3"},{"type":"html","content":"<span class='clj-long'>2</span>","value":"2"},{"type":"html","content":"<span class='clj-long'>1</span>","value":"1"}],"value":"[3 2 1]"}],"value":"([1 2 3] [1 3 2] [2 1 3] [2 3 1] [3 1 2] [3 2 1])"}
-;; <=
 
 ;; **
 ;;; But this is redundant: (1, 2, 3), (2, 3, 1), and (3, 1, 2) are three ways of describing the same tour. So let's arbitrarily say that all tours must start with the first city in the set of cities. We'll just pull the first city out, and then tack it back on to all the permutations of the rest of the cities.
